10bdc83c7de5cd91701838c6ea90979f662867b3
Bruno Belchior <brunovicenteb@gmail.com>
2023-02-13 10:49:01 -0300

    Add documentations on GraphQL API..

--- src/Models/Platform.cs
+++ src/Models/Platform.cs
@@ -2,12 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GraphQLPoC.Models;
 
+[GraphQLDescription("Represents any software or service that has a command line interface")]
 public class Platform
 {
     [Key]
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
+    [GraphQLDescription("Represents a purchased, valid licence for the platform")]
     public string LicenseKey { get; set; }
     public ICollection<Command> Commands { get; set; } = new List<Command>();
 }
